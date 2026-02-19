c3505196280ff933ce1684cb71c751a39b34bb26
PRIYANSHI-TECH06 <priyanshisrivastava173@gmail.com>
2026-02-19 22:35:50 +0530

    Create program3.c

---
+++ program3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    float r,area;
+    printf("enter radius:\n");
+    scanf("%f",&r);
+
+area=3.14*r*r;
+printf("area of circle=%.2f",area);
+
+
+
+
+    return 0;
+}

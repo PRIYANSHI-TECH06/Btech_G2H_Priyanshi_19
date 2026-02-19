f1ae1bbf95194c701a9a1f74714634a854582bec
PRIYANSHI-TECH06 <priyanshisrivastava173@gmail.com>
2026-02-19 23:20:19 +0530

    Create program8.c

---
+++ program8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int n;
+int i;
+
+    
+    printf("enter a number:\n");
+    scanf("%d",&n);
+
+for(i=1;i<=10;i++)
+{
+
+
+printf("%dx%d=%d\n",n,i,n*i);
+}
+return 0;
+}
